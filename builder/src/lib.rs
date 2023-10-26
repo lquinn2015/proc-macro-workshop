515308f6114047e3bbc8acde80cec875308e1f13
Luke Quinn <lquinn@vt.edu>
2023-10-26 15:20:43 -0400

    working on 3 using over defined

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,31 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as DeriveInput);
+    let name = &ast.ident;
+    let bname = format!("{name}Builder");
+    let bident = syn::Ident::new(&bname, ast.ident.span());
+    let expanded = quote! {
+        pub struct #bident {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+        impl #name {
+            fn builder() -> #bident  {
+                #bident {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    };
 
-    unimplemented!()
+    expanded.into()
 }
