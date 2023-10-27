4f1f33ab3f93279b2494c6e8c478263339d20a72
xphos <lquinn@vt.edu>
2023-10-26 23:04:13 -0400

    This was black magic

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -8,13 +8,45 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let name = &ast.ident;
     let bname = format!("{name}Builder");
     let bident = syn::Ident::new(&bname, ast.ident.span());
-    let expanded = quote! {
-        pub struct #bident {
-            executable: Option<String>,
-            args: Option<Vec<String>>,
-            env: Option<Vec<String>>,
-            current_dir: Option<String>,
+
+    //eprintln!("{:#?}", ast);
+    let fields = if let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed { ref named, .. }),
+        ..
+    }) = ast.data
+    {
+        named
+    } else {
+        unimplemented!()
+    };
+
+    let opts = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        quote! { #name: std::option::Option<#ty>}
+    });
+
+    let methods = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        quote! {
+            pub fn #name(&mut self, #name: #ty) -> &mut Self {
+                self.#name = Some(#name);
+                self
+            }
         }
+    });
+
+    let expanded = quote! {
+
+        pub struct #bident {
+            #(#opts,)*
+        }
+
+        impl #bident {
+            #(#methods)*
+        }
+
         impl #name {
             fn builder() -> #bident  {
                 #bident {
