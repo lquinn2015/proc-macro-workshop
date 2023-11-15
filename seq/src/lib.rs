1658cc7c24fc447ec343bbc921db02170ff4b974
Luke Quinn <lquinn@vt.edu>
2023-11-15 16:15:56 -0500

    added all syn features this worked

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,27 @@
 use proc_macro::TokenStream;
+use syn::parse::Parse;
+use syn::{parse_macro_input, Token};
+
+#[derive(Debug)]
+struct SeqMacroInput {}
+
+impl Parse for SeqMacroInput {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let var = syn::Ident::parse(input)?;
+        let _tin: Token![in] = input.parse()?;
+        let rang: syn::ExprRange = input.parse()?;
+        eprintln!("{:#?}", var);
+        eprintln!("{:#?}", rang);
+        let _tree: syn::ExprBlock = input.parse()?;
+
+        Ok(SeqMacroInput {})
+    }
+}
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as SeqMacroInput);
+    eprintln!("{:#?}", ast);
 
-    unimplemented!()
+    TokenStream::new()
 }
